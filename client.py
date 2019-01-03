06ea3da8aa0efef0db4ec477caa46c1eb094b5f8
2karis <gitauk2@gmail.com>
2019-01-02 19:39:34 -0500

    added loop

--- client.py
+++ client.py
@@ -6,58 +6,66 @@ from datetime import datetime, timedelta, date, time
 import json
 
 
-
-def getTimes():
-    tz = pytz.timezone("Africa/Nairobi")
-    the_date = date.today()
-    midnight_without_tzinfo = datetime.combine(the_date, time())
-    midnight_with_tzinfo = tz.localize(midnight_without_tzinfo)
-    midnight = midnight_with_tzinfo.astimezone(pytz.utc)
-    start = midnight_without_tzinfo + timedelta(hours=17, minutes=0)
-    end = midnight_without_tzinfo + timedelta(hours=17, minutes=30)
-    now = datetime.now()
-    return start, end, now
-
-
-def isNowInTimePeriod(startTime, endTime, nowTime):
-    if startTime < endTime:
-        return nowTime >= startTime and nowTime <= endTime
-    else: #Over midnight
-        return nowTime >= startTime or nowTime <= endTime
+#
+# def getTimes():
+#     tz = pytz.timezone("Africa/Nairobi")
+#     the_date = date.today()
+#     midnight_without_tzinfo = datetime.combine(the_date, time())
+#     midnight_with_tzinfo = tz.localize(midnight_without_tzinfo)
+#     midnight = midnight_with_tzinfo.astimezone(pytz.utc)
+#     start = midnight_without_tzinfo + timedelta(hours=17, minutes=0)
+#     end = midnight_without_tzinfo + timedelta(hours=17, minutes=30)
+#     now = datetime.now()
+#     return start, end, now
+#
+#
+# def isNowInTimePeriod(startTime, endTime, nowTime):
+#     if startTime < endTime:
+#         return nowTime >= startTime and nowTime <= endTime
+#     else: #Over midnight
+#         return nowTime >= startTime or nowTime <= endTime
 
 
 def isWeekDay():
     return datetime.today().weekday() < 5
 
 
+def is_in_time():
+    tz = pytz.timezone('Africa/Nairobi')
+    nairobi_now = datetime.now(tz).time()
+    nairobi_hour = nairobi_now.hour
+    nairobi_minute = nairobi_now.minute
+    set_time = time(15, 30)
+    time_hour = set_time.hour
+    time_minute = set_time.minute
+
+    if nairobi_hour == time_hour and nairobi_minute == time_minute:
+        return 1
+    return 0
 
 
-def try_stocks(start_time, end_time, now_time):
+
+def try_stocks():
+    print("crawler active")
     #if isWeekDay() and isNowInTimePeriod(start_time, end_time, now_time):
+    while 1:
+        if isWeekDay() and is_in_time():
+            f = open('config.json')
+            config = json.load(f)
+            t = ticker_data(config)
+            m = market_data(config)
 
-# if isWeekDay():
-    f = open('config.json')
-    config = json.load(f)
-    t = ticker_data(config)
-    m = market_data(config)
-
-    for s in (t, m):
-        s.login()
-        s.crawl_data()
-        print()
-    ts = tt.time()
-    st = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    print('time collected :', st)
-# else:
-#     print("bad time")
-
-    tt.sleep(86400)#900 86400
-    now_time = datetime.now()
-    print(start_time, end_time, now_time)
-    try_stocks(start_time, end_time, now_time)
+            for s in (t, m):
+                s.login()
+                s.crawl_data()
+                print()
+            tt.sleep(300)#900 86400
+    # now_time = datetime.now()
+    # print(start_time, end_time, now_time)
+    # try_stocks(start_time, end_time, now_time)
 
 
 
-startTime, endTime, nowTime = getTimes()
+# startTime, endTime, nowTime = getTimes()
 
-try_stocks(startTime, endTime, nowTime)
+try_stocks()
